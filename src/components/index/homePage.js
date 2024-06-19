c01c69a5359222b8133c414c6f1cdef417f44c8a
MilagrosSerrano <serranomilagros23@gmail.com>
2024-06-19 01:29:25 -0300

    alt img

--- src/components/index/homePage.js
+++ src/components/index/homePage.js
@@ -3,7 +3,7 @@ import fire from '../../icons/fire.png';
 function HomePage(){
     return(
         <div className='flex flex-col items-center p-10 m-16 text-purple-300'>
-            <img src={fire} className=' w-80 pb-12'/>
+            <img src={fire} alt='fireIcon' className=' w-80 pb-12'/>
             <h1 className='text-3xl p-3 text-center'>Metas App</h1>
             <h3 className='text-xl text-center'>¡Bienvenido a Nuestra App!</h3>
             <p className='text-md text-center'>Queremos ayudarte a planear y completar tus objetivos.</p>
