449006c7b0a864e9685e374b837bee15d3505af9
MilagrosSerrano <serranomilagros23@gmail.com>
2024-06-19 11:06:52 -0300

    .

--- src/components/shared/modalForm.js
+++ src/components/shared/modalForm.js
@@ -1,7 +1,7 @@
 function ModalForm({children}){
     return(
         <>
-        <div className="flex items-center fixed inset-0 bg-gray-700 bg-opacity-75">
+        <div className="flex items-center p-4 fixed inset-0 bg-gray-700 bg-opacity-75">
             <div className='mx-auto w-auto md:w-2/3'>
                {children}
             </div>
